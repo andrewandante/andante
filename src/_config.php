eca80bacc373aeb824f0873f54f4a7b75ce6d111
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-02-07 11:16:42 +0000

    FIX/blog-post-perms

--- src/_config.php
+++ src/_config.php
@@ -1,6 +1,8 @@
 <?php
 
 global $project;
+use SilverStripe\Control\Director;
+
 $project = 'mysite';
 
 global $database;
@@ -9,3 +11,8 @@ $database = '';
 require_once('conf/ConfigureFromEnv.php');
 
 \SilverStripe\ORM\Search\FulltextSearchable::enable();
+
+if (Director::isLive()) {
+	Director::forceSSL();
+	Director::forceWWW();
+}
